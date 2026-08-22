f77b3d8e62eafb378e53e8a1cf4b0c1bb0da66f1
mugeswarangokulraj <mugeswaran601@gmail.com>
2026-08-22 10:16:33 +0530

    PGM-4&5 RDBMS 1U25IT059

--- solution.sql
+++ solution.sql
@@ -1,2 +1,19 @@
+CREATE DATABASE CollegeDB;
+USE CollegeDB;
+CREATE TABLE Course (CourseID INT(10) PRIMARY KEY,
+                     CourseName VARCHAR(30),
+                     Creits INT(10),
+                     Department VARCHAR (30)
+                    );
+INSERT INTO Course
+VALUES ((20200011,'INFORMATION TECHNOLOGY',95,'BSC IT'),
+       (20205616,'COMPUTER SCIENCE',90,'BSC CS'),
+       (20205523,'INFORMATION TECHNOLOGY',85,'BCA IT'));
+DESC Course;
 
-
+CREATE TABLE Student ( StudentID INT (10) PRIMARY KEY,StudentNAME VARCHAR (20),Gender VARCHAR (20),DeparmentID INT (20) );
+INSERT INTO Student
+VALUE ((100,'Arun','Male',2002),
+      (101,'Divya','Female',2001),
+      (102,'Karthik','male',2003));
+DESC Student;
